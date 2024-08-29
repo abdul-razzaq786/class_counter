3533b2536dceace67779522837c12644029589f9
abdul razzaq <abdulrazzaq@gmail.com>
2024-08-29 14:51:07 +0530

    test commit

---
+++ classcounter/src/counter.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handleClick = (value) => {
+    this.setState((prevState) => {
+        return {count: prevState.count + value};
+    })
+  };
+
+  render() {
+    return (
+      <>
+      <div style={{
+        justifyContent:"center",
+        textAlign:"center"
+    }}>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={() => this.handleClick(1)}>Increment</button>
+        <button onClick={() => this.handleClick(-1)}>Decrement</button>
+        </div>
+      </>
+    );
+  }
+}
